fb55e383fe798f8dab609cbd44561791f506516d
Firat <frtgurgur@gmail.com>
2019-10-05 22:35:30 +0300

    edit

--- app/src/main/java/com/gurgur/haberinvarmi/Fragment_Doviz.java
+++ app/src/main/java/com/gurgur/haberinvarmi/Fragment_Doviz.java
@@ -1,20 +1,39 @@
 package com.gurgur.haberinvarmi;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class Fragment_Doviz extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_doviz,container,false);
 
+
+
+
+
         return view;
     }
+
 
 }
--- app/src/main/java/com/gurgur/haberinvarmi/MainActivity.java
+++ app/src/main/java/com/gurgur/haberinvarmi/MainActivity.java
@@ -7,11 +7,24 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
     Toolbar toolbar;
+    ProgressDialog pd;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,5 +79,9 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.replace(R.id.frame_kategori,fragment);
         fragmentTransaction.commit();
     }
+
+
+
+
 
 }
